94ec3d83135584f4be8b767fc8f0d789ee9c7b58
johelmejias <jomejiasma@est.utn.ac.cr>
2026-06-06 19:05:31 -0600

    Actualización y mejora de la interfaz del menú principal
    Se realiza una refactorización de la escena `menuPrincipal.tscn` para mejorar la experiencia de usuario.
    
    Los cambios clave incluyen:
    - Incorporación de nuevas fuentes (Montserrat) y una imagen de fondo (`marco_cafe_v2.png`).
    - Implementación de estilos personalizados (`StyleBoxFlat`) para los botones en sus estados normal, presionado y hover.
    - Reestructuración de la forma en que se muestran los íconos de los botones.
    - Adición de una nueva sección `Seleccion_Juego` que se activa con una animación de transición al presionar el botón "Jugar".

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,10 +1,18 @@
 extends Control
+@onready var boton_jugar_menu = $Menu_Botones/Jugar
+@onready var boton_reglas_menu = $Menu_Botones/Reglas
+@onready var boton_creditos_menu = $Menu_Botones/Creditos
 @onready var boton_salir_menu = $Menu_Botones/Salir
+@onready var menu_botones: Control = $Menu_Botones
+@onready var seleccion_juego: Control = $Seleccion_Juego
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	boton_salir_menu.pressed.connect (
 		salir
+	)
+	boton_jugar_menu.pressed.connect(
+		_on_jugar_pressed
 	)
 
 func salir() -> void:
@@ -13,3 +21,8 @@ func salir() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_jugar_pressed() -> void:
+	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(menu_botones, "position:x", 100.0, 0.6)
+	tween.tween_property(seleccion_juego, "position:x", 650.0, 0.6)
