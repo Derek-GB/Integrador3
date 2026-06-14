c95769ee1d354e22054d8de68a5bb3cbdacfbb84
johelmejias <jomejiasma@est.utn.ac.cr>
2026-06-13 19:48:55 -0600

    Creación del Autoload AudioManager
    Se crea el audioManager que controla la musica y sfx en cualquier escena del juego y se sincroniza con el soundControl

--- scripts/UX/SoundControl.gd
+++ scripts/UX/SoundControl.gd
@@ -1,39 +1,31 @@
 extends Control
-
+const MUSIC_BUS = "Music"
+const SFX_BUS = "SFX"
+const MASTER_BUS = "Master"
 @onready var btn_music_control: HSlider = $Music
 @onready var btn_sfx_control: HSlider = $SFX
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	synchronize_slider(MASTER_BUS,btn_music_control)
+		
 	btn_music_control.value_changed.connect(
 		func (value):
-			change_volume("Music", value)
+		AudioManager.change_volume(MASTER_BUS, value)
 	)
 	
 	btn_sfx_control.value_changed.connect(
 		func (value):
-			change_volume("SFX", value)
+			AudioManager.change_volume("SFX", value)
 	)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func change_volume(name_bus: String, linear_value: float) -> void:
-	# 1. Obtenemos el número de índice del bus mediante su nombre
-	var bus_index = AudioServer.get_bus_index(name_bus)
+func synchronize_slider(name_bus: String, slider:HSlider) -> void:
+	var bus_idx = AudioServer.get_bus_index(name_bus)
+	var linear_vol = db_to_linear(AudioServer.get_bus_volume_db(bus_idx))
+	slider.value = linear_vol
 	
-	# Caso límite: Si el nombre está mal escrito, el índice será -1. Evitamos un crash.
-	if bus_index == -1:
-		push_error("El bus de audio '" + name_bus + "' no existe.")
-		return
-		
-	# 2. Si el valor es 0, silenciamos el bus por completo (Mute) para ahorrar procesamiento
-	if linear_value <= 0.0:
-		AudioServer.set_bus_mute(bus_index, true)
-	else:
-		AudioServer.set_bus_mute(bus_index, false)
-		# 3. Convertimos el porcentaje (0.0 a 1.0) a Decibelios matemáticos (-60dB a 0dB)
-		var volume_db = linear_to_db(linear_value)
-		AudioServer.set_bus_volume_db(bus_index, volume_db)
