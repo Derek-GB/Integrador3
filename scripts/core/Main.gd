b3d5665df66aa2f0fbdae91211796781cfea85f4
Charly <cagonzalezob@est.utn.ac.cr>
2026-06-29 14:22:06 -0600

    Terminado-Modificaciones Bot
    - show_blue_card(): La carta siempre se va a mostrar durante 2 segundos, lo mismo pasa con redcard.
    
    launch_minigame_for_tile(): el minigame se lanza pero se resulve con un 50/50, para que noo se tenga que ver la animacion y perder tiempo

--- scripts/core/Main.gd
+++ scripts/core/Main.gd
@@ -380,7 +380,7 @@ func _apply_skip(player_index: int) -> void:
 		GameManager._next_turn()
 
 func _machine_turn() -> void:
-	await get_tree().create_timer(1.2).timeout
+	await get_tree().create_timer(0.3).timeout
 	if game_over:
 		return
 	dice_label.text = "La Maquina esta tirando el dado..."
